b24b7e66be044a99b304da18774232058dbf7825
m0rozov <andrey.morozov@jetrockets.ru>
2019-12-05 15:43:19 +0300

    Edited travis

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 ActiveRecord::Schema.define version: 0 do
-  create_table :users, force: true do |t|
-    t.string :name, null: false
-    t.timestamp 'created_at'
-    t.timestamp 'updated_at'
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.timestamp "created_at", null: false
+    t.timestamp "updated_at", null: false
   end
 
-  create_table :posts, force: true do |t|
-    t.integer :user_id, null: false
-    t.string :title, null: false
-    t.string :tags, array: true
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id", null: false
+    t.string "tags", array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
