b951ded93febc45cefea8c618f10d3db00b6f41a
Eugene Komissarov <enoch0x5a@gmail.com>
2020-04-29 12:50:38 +0300

    Changed multi-column support

--- lib/metka/model.rb
+++ lib/metka/model.rb
@@ -3,23 +3,24 @@
 require 'arel'
 
 module Metka
-  TAGGED_COLUMN_NAMES = []
   OR = Arel::Nodes::Or
   AND = Arel::Nodes::And
 
-  def self.Model(column:, **options)
-    TAGGED_COLUMN_NAMES << column unless TAGGED_COLUMN_NAMES.include?(column)
-    Metka::Model.new(column: column, **options)
+  def self.Model(column: nil, columns: nil, **options)
+    columns = [column, *columns].uniq.compact
+    raise ArgumentError, 'Columns not specified' unless columns.present?
+
+    Metka::Model.new(column: column, columns: columns, **options)
   end
 
   class Model < Module
-    def initialize(column:, **options)
-      @column = column
-      @options = options
+    def initialize(columns: nil, **options)
+      @columns = columns.dup.freeze
+      @options = options.dup.freeze
     end
 
     def included(base)
-      column = @column
+      columns = @columns
       parser = ->(tags) {
         @options[:parser] ? @options[:parser].call(tags) : Metka.config.parser.instance.call(tags)
       }
@@ -38,20 +39,20 @@ module Metka
       # @returns ViewPost::ActiveRecord_Relation
       tagged_with_lambda = ->(model, tags, **options) {
         parsed_tag_list = parser.call(tags)
+
         return model.none if parsed_tag_list.empty?
 
-        request = ::Metka::QueryBuilder.new.call(model,
-          ::Metka::TAGGED_COLUMN_NAMES,
-          parsed_tag_list, options)
+        request = ::Metka::QueryBuilder.new.call(model, columns, parsed_tag_list, options)
         model.where(request)
       }
 
       base.class_eval do
-        scope "with_all_#{column}", ->(tags) { search_by_tags.call(self, tags, column) }
-        scope "with_any_#{column}", ->(tags) { search_by_tags.call(self, tags, column, {any: true}) }
-        scope "without_all_#{column}", ->(tags) { search_by_tags.call(self, tags, column, {exclude: true}) }
-        scope "without_any_#{column}", ->(tags) { search_by_tags.call(self, tags, column, {any: true, exclude: true}) }
-
+        columns.each do |column|
+          scope "with_all_#{column}", ->(tags) { search_by_tags.call(self, tags, column) }
+          scope "with_any_#{column}", ->(tags) { search_by_tags.call(self, tags, column, {any: true}) }
+          scope "without_all_#{column}", ->(tags) { search_by_tags.call(self, tags, column, {exclude: true}) }
+          scope "without_any_#{column}", ->(tags) { search_by_tags.call(self, tags, column, {any: true, exclude: true}) }
+        end
 
         unless respond_to?(:tagged_with)
           scope :tagged_with, ->(tags = '', options = {}) {
@@ -63,13 +64,15 @@ module Metka
         end
       end
 
-      base.define_method(column.singularize + '_list=') do |v|
-        write_attribute(column, parser.call(v).to_a)
-        write_attribute(column, nil) if send(column).empty?
-      end
+      columns.each do |column|
+        base.define_method(column.singularize + '_list=') do |v|
+          write_attribute(column, parser.call(v).to_a)
+          write_attribute(column, nil) if send(column).empty?
+        end
 
-      base.define_method(column.singularize + '_list') do
-        parser.call(send(column))
+        base.define_method(column.singularize + '_list') do
+          parser.call(send(column))
+        end
       end
     end
   end
